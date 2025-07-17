ac57aff33cb797d82da9707f36e151e0097fdfa1
Gatlat Both <gatlatboth@gmail.com>
2025-07-17 13:14:28 +0300

    the changes

--- alx_backend_security/settings.py
+++ alx_backend_security/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'alx_backend_security.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'alx_airbnb_database',
+        'USER': 'root',
+        'PASSWORD': 'Faithoverfear@1998',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
